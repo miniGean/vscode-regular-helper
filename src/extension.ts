d4799b7d5f530d824636b39e5a75243006ca3c78
yangxuejing <yangxuejing_96@163.com>
2020-01-12 20:58:29 +0800

    feat: 添加快捷键调用 & 功能完善 & 更新文档

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import REGULARS from './regulars'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,11 +14,55 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.reg-ip', () => {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('regular-helper.search', () => {
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/');
+		interface regularItem {
+			label: String,
+			regular: RegExp
+		}
+		const editorInsertReg = function ({ label, regular }: regularItem): void {
+			const editor = vscode.window.activeTextEditor;
+			if (editor) {
+				const { selections } = editor;
+
+				editor.edit(editBuilder => {
+					selections.forEach(selection => {
+						const { start, end } = selection;
+						const range = new vscode.Range(start, end);
+						editBuilder.replace(range, String(regular));
+					});
+				});
+				// Display a message box to the user
+				vscode.window.showInformationMessage('正则已插入。');
+			} else {
+				vscode.window.showWarningMessage('只有在编辑文本的时候才可以使用。');
+			}
+		}
+		
+		// 展示一个快速选择面板
+		interface CommandQuickPickItem extends vscode.QuickPickItem {
+			regular: RegExp,
+			command: Function
+		}
+
+		let items: CommandQuickPickItem[] = [];
+		items = REGULARS.map(item => {
+			return {
+				label: item.label,
+				description: item.type,
+				detail: String(item.regular),
+				regular: item.regular,
+				command: editorInsertReg
+			}
+		})
+
+		vscode.window.showQuickPick(items, { matchOnDetail: true, matchOnDescription: true }).then(selectedItem => {
+			// 选中正则回调
+			if (selectedItem && typeof selectedItem.command === 'function') {
+				// 插入到文件中
+					selectedItem.command(selectedItem);
+			}
+		})
 	});
 
 	context.subscriptions.push(disposable);
